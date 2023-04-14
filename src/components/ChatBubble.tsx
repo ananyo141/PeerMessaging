129ccc89e99c9befe255eb2d8b493225d9c41469
Ananyobrata Pal <ananyo141@gmail.com>
2023-04-14 19:19:56 +0530

    feat:  - add daisyui  - create component for chat bubble

---
+++ src/components/ChatBubble.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+type Props = {
+  sender: string;
+  message: string;
+  status: string;
+  time: string;
+  self: boolean;
+};
+
+const ChatBubble = (props: Props) => {
+  return (
+    <div className={props.self ? "chat chat-end" : "chat chat-start"}>
+      <div className="chat-header">
+        {props.sender}
+        <time className="text-xs opacity-50">{props.time}</time>
+      </div>
+      <div className="chat-bubble">{props.message}</div>
+      <div className="chat-footer opacity-50">{props.status}</div>
+    </div>
+  );
+};
+
+export default ChatBubble;
